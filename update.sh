4e76626e5644cbe320aa02f77110afab5f86842e
Alexander Schindler <schindler89@aol.com>
2018-12-20 11:36:38 +0100

    Rework update shell

--- update.sh
+++ update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-curl /var/run/docker.sock -X POST "http://68.183.72.62:5555/containers/test/stop"
-curl /var/run/docker.sock -X POST "http://68.183.72.62:5555/containers/prune"
-curl /var/run/docker.sock -X POST "http://68.183.72.62:5555/images/create?fromImage=klibio/io.klib.docker.osgi.tutorial:master-latest"
-curl /var/run/docker.sock -H "Content-Type: application/json" -d '{"Image": "klibio/io.klib.docker.osgi.tutorial:master-latest",}' -X POST "http://68.183.72.62:5555/containers/create"
+curl /var/run/docker.sock \
+ -X POST "http://68.183.72.62:5555/containers/test/stop" \
+ -X POST "http://68.183.72.62:5555/containers/prune" \
+ -X POST "http://68.183.72.62:5555/images/create?fromImage=klibio/io.klib.docker.osgi.tutorial:master-latest" \
+ -H "Content-Type: application/json" \
+ --data @- <<END;
+ {
+     "Image": "klibio/io.klib.docker.osgi.tutorial:master-latest"
+     } END \
+ -X POST "http://68.183.72.62:5555/containers/create"

c7969d47d3475eba4130f96865083dbe892ac405
Alexander Schindler <schindler89@aol.com>
2019-01-31 13:31:22 +0100

    Fix install script

--- install_azure.sh
+++ install_azure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-- sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
-- sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
-- echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-- sudo apt-get update -qq
-- sudo apt-get install azure-cli
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update -qq
+sudo apt-get install azure-cli

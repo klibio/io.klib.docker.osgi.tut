526fe55c0d41b0313d00f8fb1004c4e063bbbddd
TheECanyon <schindler89@aol.com>
2019-04-03 11:12:00 +0200

    adding registry description
    added how to start a registry locally to the notes

--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-docker container run -it -v c:\Git\ENGINE_LIB_DIR:/ENGINE_LIB_DIR:ro -v c:/Git/io.klib.docker.osgi.tut/:/io.klib.doc
-ker.osgi.tut -e "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/ENGINE_LIB_DIR/jdk/bin:/ENGINE_LIB_DIR/Maven/bin" -e "JA
-VA_HOME=/ENGINE_LIB_DIR/jdk/" --name ibuntu ubuntu sh -c "cd io.klib.docker.osgi.tut && mvn verify"
+docker container run \
+-it \
+-v c:/Git/ENGINE_LIB_DIR:/ENGINE_LIB_DIR:ro \
+-v c:/Git/io.klib.docker.osgi.tut/:/io.klib.docker.osgi.tut \
+-e "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/ENGINE_LIB_DIR/jdk/bin:/ENGINE_LIB_DIR/Maven/bin" \
+-e "JAVA_HOME=/ENGINE_LIB_DIR/jdk/" \
+--name ibuntu \
+ubuntu \
+sh -c "cd io.klib.docker.osgi.tut && mvn verify"
+docker rm -f ibuntu
